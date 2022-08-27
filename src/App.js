bc37b2eb06c495360e31bfbc5c1a95fa941e9291
박은수 <es10130813@sookmyung.ac.kr>
2022-08-28 03:48:47 +0900

    message

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+import "./App.css"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot 
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+}
+
+
+
+
+export default App;
